6f2ae886ae4fd063452f79439649fc979c8180d0
tansantktk <tansantktk@hotmail.co.jp>
2022-03-26 02:42:12 +0900

    fix: cors setting

--- src/main/java/com/birdseyeapi/birdseyeapi/BirdseyeapiApplication.java
+++ src/main/java/com/birdseyeapi/birdseyeapi/BirdseyeapiApplication.java
@@ -13,18 +13,19 @@ public class BirdseyeapiApplication {
 		SpringApplication.run(BirdseyeapiApplication.class, args);
 	}
 	
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// 	return new WebMvcConfigurer() {
-	// 		@Override
-	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 			registry
-	// 				.addMapping("/**")
-	// 				.allowedOrigins(
-	// 					"http://localhost:3000", 
-	// 					"http://126.85.37.36:3000",
-	// 					"https://birds-eye.ts-soda.net2");
-	// 		}
-	// 	};
-	// }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins(
+						"http://localhost:3000", 
+						"https://birds-eye.ts-soda.net")
+					.allowedHeaders("*")
+					.allowedMethods("*");
+			}
+		};
+	}
 }
