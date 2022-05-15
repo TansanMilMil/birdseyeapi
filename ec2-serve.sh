1205fe8825f64b21c60ca4aff3f84d0454cc2ef7
tansantktk <tansantktk@hotmail.co.jp>
2022-05-15 16:00:45 +0900

    add: circleci settings

--- ec2-serve.sh
+++ ec2-serve.sh
@@ -1,7 +1,8 @@
 #/bin/bash
-
+set -eu
 cd `dirname $0`
+
 echo -e '\e[36mEC2: restart docker-compose...\e[m'
-docker-compose down && \
-docker-compose build && \
+docker-compose down
+docker-compose build
 docker-compose up -d
