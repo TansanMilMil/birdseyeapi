d73a6c8aece294016f22e815c9905d7f9f287a80
tansantktk <tansantktk@hotmail.co.jp>
2022-05-15 12:41:53 +0900

    update: prevent memory reak

--- src/main/java/com/birdseyeapi/birdseyeapi/ScrapingReaction/ScrapeReactionsByTwitter.java
+++ src/main/java/com/birdseyeapi/birdseyeapi/ScrapingReaction/ScrapeReactionsByTwitter.java
@@ -24,29 +24,33 @@ public class ScrapeReactionsByTwitter {
         
         DesiredCapabilities firefox = DesiredCapabilities.firefox();
         WebDriver driver = new RemoteWebDriver(new URL(System.getenv("SELENIUM_URL")), firefox);
-        LOG.info("selenium is ready.");
-        driver.get("https://twitter.com/search?f=tweets&vertical=default&q=" + url);
-        LOG.info("selenium is requesting twitter.");
-        Thread.sleep(1000);
-        LOG.info("request completed.");
-        
-        List<WebElement> articles = driver.findElements(By.cssSelector("#react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > div > section > div > div > div > div > div > article > div > div > div > div > div > div:nth-child(2) > div:nth-child(1)"));
-        LOG.info("articles.size(): " + articles.size());
-        for (WebElement article : articles) {
-            String text = article.getText();
-            if (text == null || text.trim().isEmpty() || text.equals(title)) {
-                continue;
+        try {
+            LOG.info("selenium is ready.");
+            driver.get("https://twitter.com/search?f=tweets&vertical=default&q=" + url);
+            LOG.info("selenium is requesting twitter.");
+            Thread.sleep(1000);
+            LOG.info("request completed.");
+            
+            List<WebElement> articles = driver.findElements(By.cssSelector("#react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > div > section > div > div > div > div > div > article > div > div > div > div > div > div:nth-child(2) > div:nth-child(1)"));
+            LOG.info("articles.size(): " + articles.size());
+            for (WebElement article : articles) {
+                String text = article.getText();
+                if (text == null || text.trim().isEmpty() || text.equals(title)) {
+                    continue;
+                }
+                LOG.info("-------------------------");
+                LOG.info(text);
+                NewsReaction reaction = new NewsReaction();
+                reaction.author = "twitter user";
+                reaction.comment = text;
+                reaction.scrapedDateTime = now;
+                reactions.add(reaction);
             }
-            LOG.info("-------------------------");
-            LOG.info(text);
-            NewsReaction reaction = new NewsReaction();
-            reaction.author = "twitter user";
-            reaction.comment = text;
-            reaction.scrapedDateTime = now;
-            reactions.add(reaction);
+        } finally {
+            driver.quit();
+            LOG.info("selenium quit.");
         }
-        driver.quit();
-        LOG.info("selenium quit.");
         return reactions;
+    
     }
 }
