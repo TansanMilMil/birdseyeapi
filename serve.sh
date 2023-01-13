dd02a6bfe5d316a06c0bbb72382ef7ea0e2c902f
TansanMilMil <tmdev2929@gmail.com>
2023-01-13 23:49:07 +0900

    update: application.properties

--- serve.sh
+++ serve.sh
@@ -4,8 +4,6 @@ cd `dirname $0`
 docker compose down && \
 docker compose up -d && \
 
-docker compose exec mysql mysql -u root -proot1603 -e 'CREATE DATABASE IF NOT EXISTS birds_eye'
-
 docker compose exec java ./gradlew build && \
 # use application-dev.properties
 docker compose exec java java -jar -Dspring.profiles.active=dev ./build/libs/birdseyeapi-0.0.1-SNAPSHOT.jar
