ac983825bfd969c2936d8788bfa73f7d27646cee
TansanMilMil <tmdev2929@gmail.com>
2023-04-24 21:04:09 +0900

    add: unit test

---
+++ src/test/java/com/birdseyeapi/birdseyeapi/HogeServiceTest.java
@@ -0,0 +1,25 @@
+package com.birdseyeapi.birdseyeapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HogeServiceTest {
+
+    @Autowired
+    public HogeService hogeService;
+
+    @Test
+    public void testDoSomething() {
+        hogeService = spy(hogeService);
+
+        hogeService.doSomething();
+        verify(hogeService, times(1)).doSomething();
+    }
+}
